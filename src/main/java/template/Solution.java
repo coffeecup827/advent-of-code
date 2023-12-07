7f7c647e10c087da17f1a1099f6c4266bb5fdff9
coffeecup827 <coffeecup827@gmail.com>
2023-12-07 18:33:20 +0530

    Adds Base Template

---
+++ src/main/java/template/Solution.java
@@ -0,0 +1,41 @@
+package template;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+  private static final String DAY = "1";
+  private static final String PART = "1";
+  private static final Logger logger = Logger.getLogger(Solution.class.getName());
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = null;
+    try {
+      Path path = Paths.get("src/main/java/day" + DAY + "/part" + PART + "/input.txt");
+
+      reader = Files.newBufferedReader(path);
+      ArrayList<String> inputs = reader.lines().collect(Collectors.toCollection(ArrayList::new));
+      solution(inputs);
+    } catch (IOException e) {
+      logger.warning("Failed to read file");
+      e.printStackTrace();
+    } finally {
+      if (reader != null) {
+        reader.close();
+      }
+    }
+  }
+
+  private static void solution (ArrayList<String> inputs) {
+    logger.info(inputs.get(0));
+  }
+
+}
